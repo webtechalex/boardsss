b1138aa864bbacec19702f4171a43405b0c1583b
Alex Brown <alexanderbrown.info@gmail.com>
2019-01-22 20:23:54 +0000

    complete oauth configuration

--- server/index.js
+++ server/index.js
@@ -11,12 +11,18 @@ passport.use(new GoogleStrategy(
     clientSecret: process.env.CLIENT_SECRET,
     callbackURL: '/auth/google/callback'
   }, 
-  (accessToken) => console.log(accessToken)
+  (accessToken, refreshToken, profile, done) => {
+    console.log('access token', accessToken)
+    console.log('refresh token', refreshToken)
+    console.log('profile', profile)
+  }
 ))
 
 app.get('/auth/google', passport.authenticate('google', {
   scope: ['profile', 'email']
 }))
+
+app.get('/auth/google/callback', passport.authenticate('google'))
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT)
