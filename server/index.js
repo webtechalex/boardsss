95f70f5610d2339860ce389f562b3dbb3403bc4a
Alex Brown <alexanderbrown.info@gmail.com>
2019-01-22 19:28:56 +0000

    get OAuth credentials

--- server/index.js
+++ server/index.js
@@ -1,9 +1,12 @@
 const express = require('express')
+const passport = require('passport')
+const GoogleStrategy = require('passport-google-oauth20').Strategy
+
 const app = express()
 
-app.get('/', function(req, res) {
-  res.send('test...')
-})
+passport.use(new GoogleStrategy())
 
 const PORT = process.env.PORT || 3000
-app.listen(PORT);
+app.listen(PORT)
+
+// http://localhost:5000/auth/google/callback
