343c36167673fe2d80de3db7d0da0add474ba335
Alex Brown <alexanderbrown.info@gmail.com>
2019-01-22 20:04:21 +0000

    install dotenv and configure passport

--- server/index.js
+++ server/index.js
@@ -1,12 +1,22 @@
+require('dotenv').config()
 const express = require('express')
 const passport = require('passport')
 const GoogleStrategy = require('passport-google-oauth20').Strategy
 
 const app = express()
 
-passport.use(new GoogleStrategy())
+passport.use(new GoogleStrategy(
+  {
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: '/auth/google/callback'
+  }, 
+  (accessToken) => console.log(accessToken)
+))
+
+app.get('/auth/google', passport.authenticate('google', {
+  scope: ['profile', 'email']
+}))
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT)
-
-// http://localhost:5000/auth/google/callback
