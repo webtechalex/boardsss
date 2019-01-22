5db71df2aadcab05d5795408092580f1106e5598
Alex Brown <alexanderbrown.info@gmail.com>
2019-01-22 21:53:03 +0000

    refactor server

--- server/index.js
+++ server/index.js
@@ -1,28 +1,9 @@
 require('dotenv').config()
+require('./services/passport')
 const express = require('express')
-const passport = require('passport')
-const GoogleStrategy = require('passport-google-oauth20').Strategy
 
 const app = express()
-
-passport.use(new GoogleStrategy(
-  {
-    clientID: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: '/auth/google/callback'
-  }, 
-  (accessToken, refreshToken, profile, done) => {
-    console.log('access token', accessToken)
-    console.log('refresh token', refreshToken)
-    console.log('profile', profile)
-  }
-))
-
-app.get('/auth/google', passport.authenticate('google', {
-  scope: ['profile', 'email']
-}))
-
-app.get('/auth/google/callback', passport.authenticate('google'))
+require('./routes/authRoutes')(app)
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT)
